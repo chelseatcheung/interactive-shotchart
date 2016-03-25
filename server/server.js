8b633c631af68054fdff375c646fbd0eb28dd9a7
chelsea cheung <chelseatcheung@gmail.com>
2016-03-24 18:15:58 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ require('./routes/routes.js');
    var season = "SeasonFour";
    var game;
    // loop through the files in folder
-   for(var i=39;i <= 59; i++) {
+   for(var i=60;i <= 80; i++) {
      game = files[i].substring(9,15);
      // read through the content of the files (an array of objects)
      (function(position,contest){
