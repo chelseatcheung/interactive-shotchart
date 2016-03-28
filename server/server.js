ce495cc63e52825213e24a5388fa09f200ccc902
chelsea cheung <chelseatcheung@gmail.com>
2016-03-28 15:21:33 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -19,29 +19,29 @@ require('./routes/routes.js');
 //had to add them in increments or else server would crash, hence the specific number in the for loop
 
 
-(function createDocs() {
-  fs.readdir(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season'), function(err,files){
-   var season = "SeasonFour";
-   var game;
-   // loop through the files in folder
-   for(var i=238;i <= 251; i++) {
-     game = files[i].substring(9,15);
-     // read through the content of the files (an array of objects)
-     (function(position,contest){
-       fs.readFile(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season/'+files[position]), 'utf8', function (err, data) {
-          if(err) {
-           throw err;
-          } else {
-           arrayOfObj = JSON.parse(data);
-           for(var j=0; j < arrayOfObj.length; j++) {
-             createNewEvent(arrayOfObj[j], season, contest);
-           }
-          }
-       })
-     })(i,game);
-   }
-  })
-})();
+// (function createDocs() {
+//   fs.readdir(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season'), function(err,files){
+//    var season = "SeasonFour";
+//    var game;
+//    // loop through the files in folder
+//    for(var i=238;i <= 251; i++) {
+//      game = files[i].substring(9,15);
+//      // read through the content of the files (an array of objects)
+//      (function(position,contest){
+//        fs.readFile(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season/'+files[position]), 'utf8', function (err, data) {
+//           if(err) {
+//            throw err;
+//           } else {
+//            arrayOfObj = JSON.parse(data);
+//            for(var j=0; j < arrayOfObj.length; j++) {
+//              createNewEvent(arrayOfObj[j], season, contest);
+//            }
+//           }
+//        })
+//      })(i,game);
+//    }
+//   })
+// })();
 
 // (function testing () {
 //   Event.collection.stats(function(err, results) {
@@ -74,7 +74,7 @@ var findFromDB = function (season) {
     console.log('results is', result);
    };
   }).count({},function(err,count){console.log('count is ',count)});
-};
+}();
 
 
 
@@ -92,5 +92,5 @@ var removeSeason = function () {
 
 
 app.listen(PORT, function() {
-  console.log('listening on port 3000...')
+  console.log('listening on port '+ PORT );
 })
