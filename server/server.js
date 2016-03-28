06b1938ccedf105f0f041d7a3e151da08dca2fcc
chelsea cheung <chelseatcheung@gmail.com>
2016-03-28 16:24:34 -0700

    [fix] checking database

--- server/server.js
+++ server/server.js
@@ -17,37 +17,29 @@ app.use(express.static(path.join(__dirname,'../')));
 //had to add them in increments or else server would crash, hence the specific number in the for loop
 
 
-(function createDocs() {
-  fs.readdir(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season'), function(err,files){
-   var season = "SeasonFour";
-   var game;
-   // loop through the files in folder
-   for(var i=39;i <= 59; i++) {
-     game = files[i].substring(9,15);
-     // read through the content of the files (an array of objects)
-     (function(position,contest){
-       fs.readFile(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season/'+files[position]), 'utf8', function (err, data) {
-          if(err) {
-           throw err;
-          } else {
-           arrayOfObj = JSON.parse(data);
-           for(var j=0; j < arrayOfObj.length; j++) {
-             createNewEvent(arrayOfObj[j], season, contest);
-           }
-          }
-       })
-     })(i,game);
-   }
-  })
-})();
-
-// (function testing () {
-//   Event.collection.stats(function(err, results) {
-//     if(err) {throw err} else {
-//       console.log('results are ', results)
-//     }
+// (function createDocs() {
+//   fs.readdir(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season'), function(err,files){
+//    var season = "SeasonFour";
+//    var game;
+//    // loop through the files in folder
+//    for(var i=39;i <= 59; i++) {
+//      game = files[i].substring(9,15);
+//      // read through the content of the files (an array of objects)
+//      (function(position,contest){
+//        fs.readFile(path.join(__dirname,'../client/assets/parsed-data/2009-2010.regular_season/'+files[position]), 'utf8', function (err, data) {
+//           if(err) {
+//            throw err;
+//           } else {
+//            arrayOfObj = JSON.parse(data);
+//            for(var j=0; j < arrayOfObj.length; j++) {
+//              createNewEvent(arrayOfObj[j], season, contest);
+//            }
+//           }
+//        })
+//      })(i,game);
+//    }
 //   })
-// })()
+// })();
 
 //helper function that creates new instance of schema
 var createNewEvent = function(obj,seas,gam) {
@@ -72,7 +64,7 @@ var findFromDB = function (season) {
     console.log('results is', result);
    };
   }).count({},function(err,count){console.log('count is ',count)});
-};
+}();
 
 
 
