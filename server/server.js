1681bcf00391e794ee643f262757b2759b708f32
chelsea cheung <chelseatcheung@gmail.com>
2016-03-28 17:07:50 -0700

    [fix] adding to db

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ var createDocs = function() {
    var season = "SeasonFour";
    var game;
    // loop through the files in folder
-   for(var i=123;i <= 143; i++) {
+   for(var i=144;i <= 164; i++) {
      game = files[i].substring(9,15);
      // read through the content of the files (an array of objects)
      (function(position,contest){
@@ -40,7 +40,7 @@ var createDocs = function() {
    }
    console.log('DONE, SON!!!')
   })
-};
+}();
 
 //helper function that creates new instance of schema
 var createNewEvent = function(obj,seas,gam) {
@@ -65,7 +65,7 @@ var findFromDB = function (season) {
     console.log('results is');
    };
   }).count({},function(err,count){console.log('count is ',count)});
-}();
+};
 
 
 
